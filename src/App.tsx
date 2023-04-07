5d5c04872d2965aa434e288a1151005796217fd8
Richard Garner <richard.j.b.garner@gmail.com>
2023-04-08 08:40:26 +1000

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,68 @@
-import React from 'react';
-import { createBrowserRouter, ActionFunction, LoaderFunction, Outlet, Router, RouterProvider, useLoaderData, Form } from "react-router-dom"
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  createBrowserRouter,
+  ActionFunction,
+  LoaderFunction,
+  Outlet,
+  RouterProvider,
+  useLoaderData,
+  Form,
+  Link,
+  useSubmit,
+} from "react-router-dom";
 
 interface IRecipe {
-  name: string
+  id: string;
+  name: string;
 }
 
 const recipes: Record<string, IRecipe> = {
-  "a": {
-    name: "Muffins"
+  a: {
+    id: "a",
+    name: "Muffins",
   },
-  "b": {
-    name: "Waffles"
+  b: {
+    id: "b",
+    name: "Waffles",
   },
-}
+};
+
+const recipesLoader: LoaderFunction = ({ request, params, context }) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  return Object.values(recipes).filter((recipe) => {
+    return !q || new RegExp(q).test(recipe.name);
+  });
+};
 
 const recipeLoader: LoaderFunction = ({ request, params, context }) => {
-  return recipes[(params as any).id as string]
-}
+  const recipeId = (params as any).id as string;
+  return recipes[recipeId];
+};
 
 const recipeAction: ActionFunction = async ({ request, params, context }) => {
-  const recipeId = (params as any).id
-  const formData = await request.formData()
-  const data = Object.fromEntries(formData)
-  Object.assign(recipes[recipeId], data)
-  return { ok: true }
-}
+  const recipeId = (params as any).id as string;
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  Object.assign(recipes[recipeId], data);
+  return { ok: true };
+};
 
-const router = createBrowserRouter([{
-  path: "/",
-  element: <Main />,
-  children: [{
-    path: "recipe/:id",
-    element: <Recipe />,
-    loader: recipeLoader,
-    action: recipeAction
-  }]
-}])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Recipes />,
+    loader: recipesLoader,
+    children: [
+      {
+        path: "recipe/:id",
+        element: <Recipe />,
+        loader: recipeLoader,
+        action: recipeAction,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
@@ -47,23 +72,40 @@ function App() {
   );
 }
 
-function Main() {
-  return <div>
-    <h1>Recipes</h1>
-    <Outlet />
-  </div>
+function Recipes() {
+  const recipes = useLoaderData() as IRecipe[];
+  const submit = useSubmit();
+  return (
+    <div>
+      <h1>Recipes</h1>
+      <Form>
+        <input
+          name="q"
+          type="search"
+          onChange={(event) => {
+            submit(event.currentTarget.form);
+          }}
+        />
+      </Form>
+      {recipes.map((recipe) => (
+        <Link to={`recipe/${recipe.id}`}>{recipe.name}</Link>
+      ))}
+      <Outlet />
+    </div>
+  );
 }
 
 function Recipe() {
-  const recipe = useLoaderData() as IRecipe
-  return <div>
-    <h1>{recipe.name}</h1>
-    <Form method='post'>
-      <input type="text" name="name" />
-      <button type="submit">Save</button>
-    </Form>
-  </div>
+  const recipe = useLoaderData() as IRecipe;
+  return (
+    <div>
+      <h1>{recipe.name}</h1>
+      <Form method="post">
+        <input name="name" type="text" />
+        <button type="submit">Save</button>
+      </Form>
+    </div>
+  );
 }
-
 
 export default App;
